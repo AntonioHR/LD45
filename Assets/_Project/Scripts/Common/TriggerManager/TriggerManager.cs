6c300f7f1db6fdc0f3912e6f7700835f218a931d
Délio Cantarelli <delio@manifestohut.com.br>
2019-10-05 17:01:51 -0300

    add trigger manager

---
+++ Assets/_Project/Scripts/Common/TriggerManager/TriggerManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager Instance { get; private set}
+
+    private Dictionary<string, UnityEvent> events = new Dictionary<string, UnityEvent>();
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent;
+        if (Instance.events.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            Instance.events.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (Instance.events == null) return;
+        UnityEvent thisEvent;
+        if (Instance.events.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void Trigger(string eventName)
+    {
+        UnityEvent thisEvent;
+        if (Instance.events.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/SamuraiGame/Events/EventName.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Events
+{
+    public static class EventName
+    {
+		public static readonly string TestEvent = "event-name";
+    }
+}
