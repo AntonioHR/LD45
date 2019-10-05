b809ee3218a88488a5591df63d06e253d3c7f77a
Délio Cantarelli <delio@manifestohut.com.br>
2019-10-05 17:48:03 -0300

    enemy prototype

---
+++ Assets/_Project/Scripts/SamuraiGame/Enemy/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private readonly GameObject weaponHitBox;
+
+    public void Attack()
+    {
+
+    }
+}
