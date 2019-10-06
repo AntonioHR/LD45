247828e75ea60cc6cb3aadfc7c4d8956ec62165e
Antonio Harger <antonio.harger@manifestohut.com.br>
2019-10-06 20:58:25 -0300

    Setup music start trigger

---
+++ Assets/_Project/Scripts/SamuraiGame/Events/CustomTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SamuraiGame.Events
+{
+    public class CustomTrigger :MonoBehaviour
+    {
+        public UnityEvent unityEvent;
+        public string id;
+
+
+        public void Start()
+        {
+            TriggerManager.StartListening(id, unityEvent.Invoke);
+        }
+        public void DontDestroy()
+        {
+            TriggerManager.StopListening(id, unityEvent.Invoke);
+        }
+    }
+}
