a451c8c09226c78f131cb2840ca1483a0ae3b542
Antonio Harger <antonio.harger@manifestohut.com.br>
2019-10-06 17:24:20 -0300

    Added gate animation

--- Assets/_Project/Scripts/SamuraiGame/Room/GateController.cs
+++ Assets/_Project/Scripts/SamuraiGame/Room/GateController.cs
@@ -3,11 +3,46 @@ using System.Collections;
 using Common.Interactables;
 using SamuraiGame.Player;
 using SamuraiGame.Events;
+using System;
+using DG.Tweening;
 
 namespace SamuraiGame.Room
 {
     public class GateController : ObjectTrigger<PlayerController>
     {
+        public float openDistance = 1;
+        public float openTime = .5f;
+        [SerializeField]
+        private SpriteRenderer left;
+        [SerializeField]
+        private SpriteRenderer right;
+        [SerializeField]
+        private Collider2D blocker;
+        [SerializeField]
+        private Collider2D trigger;
+
+        public void Start()
+        {
+            TriggerManager.StartListening(EventName.RoomCompleted, Open);
+        }
+
+        private void Open()
+        {
+
+            var seq = DOTween.Sequence();
+            seq.Join(left.transform.DOMoveX(-openDistance, openTime).SetRelative());
+            seq.Join(right.transform.DOMoveX(openDistance, openTime).SetRelative());
+            seq.OnComplete(OnOpened);
+
+        }
+
+        private void OnOpened()
+        {
+            blocker.enabled = false;
+            trigger.enabled = true;
+
+        }
+
         protected override void OnTriggered(PlayerController obj)
         {
             TriggerManager.Trigger(EventName.OnGateEnter);
