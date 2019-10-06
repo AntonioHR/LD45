cee772e0820702cbaba6481e21d28e1d82d73b59
Délio Cantarelli <delio@manifestohut.com.br>
2019-10-06 05:06:22 -0300

    add slice feedback

---
+++ Assets/_Project/Scripts/SamuraiGame/Util/FacePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SamuraiGame.Managers;
+using SamuraiGame.Enemy;
+
+public class FacePlayer : MonoBehaviour
+{
+    private EnemyController enemyController;
+
+    private void Awake()
+    {
+        enemyController = GetComponentInParent<EnemyController>();
+
+    }
+    private void Update()
+    {
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, -enemyController.FacingDirection);
+    }
+}
