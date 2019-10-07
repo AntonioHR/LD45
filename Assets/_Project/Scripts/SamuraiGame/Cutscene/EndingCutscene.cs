af81ebafae3c3561136e3231e203e779aaaa81b5
Délio Cantarelli <delio@manifestohut.com.br>
2019-10-07 02:36:56 -0300

    add ending cutscene

---
+++ Assets/_Project/Scripts/SamuraiGame/Cutscene/EndingCutscene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using SamuraiGame.Events;
+
+public class EndingCutscene : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        TriggerManager.Trigger(EventName.OnEndingCutscen);
+    }
+
+}
